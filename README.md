fbbd1aba6ae02484a6eb22a2dad2508c2e4cb89c
William Law <williamlaw.wtl@gmail.com>
2023-07-03 16:33:36 -0700

    [docs] add readme usage

---
+++ README.md
@@ -0,0 +1,47 @@
+## Automated Gas Calibration helpers
+
+A collection of mathematical helper functions written in Python. 
+
+### Usage
+
+```python
+python3 main.py
+```
+
+### Example
+
+Consider the linear system shown in the code test:
+
+2x	+	y	−	2z	=	3
+x	−	y	−	z	=	0
+x	+	y	+	3z	=	12
+
+The Coefficient Matrix would be represented as:
+
+[
+    [1,  1,  3]
+    [1, -1, -1]
+    [2,  1, -2]
+]
+
+The Constant Vector would be represented as (a Matrix):
+
+[
+    [12]
+    [0]
+    [3]
+]
+
+The Augmented Matrix would be the matrix: Coefficient | Constant
+
+Performing RREF, we would get the following solutions:
+
+[7/2, 1, 5/2]
+
+And to detect for outliers, if we plug the computed values against the RHS
+(the benchmarked running time), we can check and account for a margin of error.
+
+Note: it is obvious that with a small sample size and integer values, plugging 
+the values back into what we just solved gives us no margin of error. However, 
+for larger systems of equations and floating points, this should offer some room
+of flexibility, in which we can start filtering values.
--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 #import numpy as np
 from sympy import *
 
+"""
+CONSTANTS
+"""
+ROW = 0
+COL = 1
+MARGIN_OF_ERROR = 0.05
+
+
 coeff_matrix = Matrix([])
 const_matrix = Matrix([])
 
@@ -45,6 +53,7 @@ def compute_least_square_solution(A, b):
     A_TA = A_T * A
     A_Tb = A_T * b
     augmented_matrix = create_augmented_matrix(A_TA, A_Tb)
+    # return value is (rref matrix, tuple of pivot columns)
     return augmented_matrix.rref()[0]
 
 
@@ -58,6 +67,7 @@ def find_free_variables(A,b):
     A_TA = A_T * A
     A_Tb = A_T * b
     augmented_matrix = create_augmented_matrix(A_TA, A_Tb)
+    # return value is (rref matrix, tuple of pivot columns)
     return augmented_matrix.rref()[1]    
 
 
@@ -113,6 +123,54 @@ def is_invertible_with_lu(M):
     return M.det(method="lu") != 0
 
 
+"""
+@notice: uses RREF values and compares against const_matrix w/ margin of error
+@param rref_matrix: Matrix
+@param coefficient_matrix: Matrix
+@param constant_matrix: Matrix
+@returns: list of tuples of gas parameters that are outliers, (i,k)
+where `i` is which row the equation is in, and `k` is the index of gas parameter
+"""
+def find_outliers(rref_matrix, coefficient_matrix, constant_matrix):
+    # get rref solutions
+    x_hat = []
+    rref_row_len = shape(rref_matrix)[ROW]
+    rref_col_pos = shape(rref_matrix)[COL]-1
+    for i in range(rref_row_len):
+        x_i = rref_matrix.row(i)[rref_col_pos]
+        x_hat.append(x_i)
+
+    # get theoretical running times using rref values
+    computed_running_time = []
+    coeff_row_len = shape(coefficient_matrix)[ROW]
+    coeff_col_len = shape(coefficient_matrix)[COL]
+    for i in range(coeff_row_len):
+        total_time = 0
+        for j in range(coeff_col_len):
+            a_ij = coefficient_matrix.row(i)[j]
+            total_time += a_ij * x_hat[j]
+        computed_running_time.append(total_time)
+    
+    # compare w/ margin of error
+    outliers = []
+    const_row_len = shape(const_matrix)[ROW]
+    for i in range(const_row_len):
+        # constant vector should always have 1 value
+        a_ij = const_matrix.row(i)[0]
+
+        # div by zero handling
+        numerator = abs(a_ij-computed_running_time[i])
+        diff = 0
+        if numerator != 0:
+            diff = abs(a_ij-computed_running_time[i]) / computed_running_time[i]
+
+        if diff > MARGIN_OF_ERROR:
+            # append all gas parameters corresponding to that equation
+            # as a cause to the outlier
+            for k in range(coeff_col_len):
+                outliers.append((i,k))
+    
+    return outliers
 
 
 """
@@ -126,14 +184,18 @@ print("===== Coefficient Matrix =====")
 #coeff_matrix = add_gas_formula_to_coefficient_matrix([1,1,0], coeff_matrix)
 #coeff_matrix = add_gas_formula_to_coefficient_matrix([1,1,1], coeff_matrix)
 
-coeff_matrix = add_gas_formula_to_coefficient_matrix([3,-1,2], coeff_matrix)
-coeff_matrix = add_gas_formula_to_coefficient_matrix([2,1,1], coeff_matrix)
-coeff_matrix = add_gas_formula_to_coefficient_matrix([1,3,0], coeff_matrix)
+#coeff_matrix = add_gas_formula_to_coefficient_matrix([3,-1,2], coeff_matrix)
+#coeff_matrix = add_gas_formula_to_coefficient_matrix([2,1,1], coeff_matrix)
+#coeff_matrix = add_gas_formula_to_coefficient_matrix([1,3,0], coeff_matrix)
+
+coeff_matrix = add_gas_formula_to_coefficient_matrix([2,1,-2], coeff_matrix)
+coeff_matrix = add_gas_formula_to_coefficient_matrix([1,-1,-1], coeff_matrix)
+coeff_matrix = add_gas_formula_to_coefficient_matrix([1,1,3], coeff_matrix)
 
 print(coeff_matrix)
 print("\n")
 
-print("===== Constant Matrix =====")
+print("===== Constant Vector =====")
 #const_matrix = add_running_time_to_constant_matrix([6], const_matrix)
 #const_matrix = add_running_time_to_constant_matrix([0], const_matrix)
 #const_matrix = add_running_time_to_constant_matrix([0], const_matrix)
@@ -141,9 +203,14 @@ print("===== Constant Matrix =====")
 #const_matrix = add_running_time_to_constant_matrix([3], const_matrix)
 #const_matrix = add_running_time_to_constant_matrix([5], const_matrix)
 
-const_matrix = add_running_time_to_constant_matrix([2], const_matrix)
-const_matrix = add_running_time_to_constant_matrix([-1], const_matrix)
-const_matrix = add_running_time_to_constant_matrix([-1], const_matrix)
+#const_matrix = add_running_time_to_constant_matrix([2], const_matrix)
+#const_matrix = add_running_time_to_constant_matrix([-1], const_matrix)
+#const_matrix = add_running_time_to_constant_matrix([-1], const_matrix)
+
+const_matrix = add_running_time_to_constant_matrix([3], const_matrix)
+const_matrix = add_running_time_to_constant_matrix([0], const_matrix)
+const_matrix = add_running_time_to_constant_matrix([12], const_matrix)
+
 print(const_matrix)
 print("\n")
 
@@ -153,9 +220,13 @@ print(aug_matrix)
 print("\n")
 
 print("===== Least Square Solution (rref) =====")
-print(compute_least_square_solution(coeff_matrix, const_matrix))
+rref = compute_least_square_solution(coeff_matrix, const_matrix)
+print(rref)
 print("\n")
 
 print("===== Least Square Solution (xhat) =====")
-print(compute_least_square_solutions(coeff_matrix, const_matrix))
+# print(compute_least_square_solutions(coeff_matrix, const_matrix))
 print("\n")
+
+print("===== Finding Outliers =====")
+print(find_outliers(rref, coeff_matrix, const_matrix))
